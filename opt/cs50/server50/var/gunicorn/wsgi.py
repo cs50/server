c89e731c07311f793284f00df521a33deb97764d
David J. Malan <malan@harvard.edu>
2016-07-31 18:20:28 -0400

    transitioning to Passenger

--- opt/cs50/server50/var/gunicorn/wsgi.py
+++
@@ -1,9 +0,0 @@
-accesslog =
-bind = "unix:/var/opt/cs50/server50/gunicorn.sock"
-chdir =
-daemon = false
-errorlog =
-group = www-data
-pid = "/var/opt/cs50/server50/gunicorn.pid"
-user = www-data
-workers = 16
